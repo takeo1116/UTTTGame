049f430ff3e30dd7098a3ac72b669404481d244a
takeo1116 <takeo1116@gmail.com>
2020-11-02 00:54:45 +0900

    Game, Boardの追加

---
+++ board.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+
+class Board:
+    # 盤面
+
+    def __init__(self):
+        self.game_board = [[0] * 9] * 9
+        print(self.game_board)
---
+++ game.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+from board import Board
+
+
+class Game:
+    # Ultimate Tic Tac Toeのゲームをシミュレートする
+
+    def __init__(self):
+        self.board = Board()
