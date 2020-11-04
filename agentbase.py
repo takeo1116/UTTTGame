c9b74da062d751b9b8d4b329c5622b11cfe5bce4
takeo1116 <takeo1116@gmail.com>
2020-11-05 00:36:34 +0900

    game実装wip

--- agentbase.py
+++ agentbase.py
@@ -1,13 +1,17 @@
 # coding:utf-8
 
-class AgentBase:
+import abc
+class AgentBase(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
     def request_move(self, board, legal, player_num):
         # 入力->盤面、合法手、自分のプレイヤー番号、出力->打つ場所
-        return legal[0]
+        pass
+    @abc.abstractmethod
     def game_end(self, board, player_num, result):
         # ゲーム終了時に呼び出される関数
         # 入力->盤面、自分のプレイヤー番号、結果、結果
         pass
+    @abc.abstractmethod
     def __init__(self):
         # エージェント生成時の処理
         pass
--- game.py
+++ game.py
@@ -9,9 +9,9 @@ class Game:
         # ゲームを一手進める
         flat_board = self.board.flatten()
         legal_moves = self.board.legal_moves()
-        
         # now_playerに手を聞く
-        move = self.players[self.now_player].request_move(flat_board.copy(), legal_moves.copy(), self.now_player)
+        move = self.players[self.now_player].request_move(
+            flat_board.copy(), legal_moves.copy(), self.now_player)
         # 返ってきた手が有効かどうか調べる（有効じゃない手を打とうとしたら負け）
         if move not in legal_moves:
             self.game_state = 1 - self.now_player
@@ -20,15 +20,20 @@ class Game:
         # ゲーム終了かどうか判定
         if self.game_state == 0:
             self.game_state = self.board.check_state()
-        # ゲーム終了なら、それぞれのエージェントに結果を返す
         if self.game_state != 0:
-            
-        # player番号を次に進める
-        self.now_player = 1 - self.now_player
+            # ゲーム終了なら、それぞれのエージェントに結果を返す
+            self.players[0].game_end(flat_board, 1, self.game_state)
+            self.players[1].game_end(flat_board, 2, self.game_state)
+        else:
+            # プレイヤーを一つ進める
+            self.now_player = 1 - self.now_player
+
+    def simulate(self):
+        # 1ゲームシミュレートする（いい関数名？）
         pass
-    
+
     def __init__(self, players):
         self.board = Board()
         self.now_player = 1
         self.players = players
-        self.game_state = 0 # 0:ゲーム進行中, 1:player1の勝ち, 2:player2の勝ち
+        self.game_state = 0  # 0:ゲーム進行中, 1:player1の勝ち, 2:player2の勝ち
