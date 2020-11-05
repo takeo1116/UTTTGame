9a6ab642f8b418343a7a0c84a9323ae634e9ad9e
takeo1116 <takeo1116@gmail.com>
2020-11-05 23:18:22 +0900

    boardの変数名変更

--- board.py
+++ board.py
@@ -1,14 +1,14 @@
 # coding:utf-8
 
-class MiniBoard:
+class LocalBoard:
     # 3×3の盤面
     def flatten(self):
         # 現在の盤面を整数のリストにする
-        return self.mini_board
+        return self.local_board
 
     def mark(self, pos, player):
         # idx番目のマス目にplayerの手を打つ
-        self.mini_board = player
+        self.local_board = player
 
     def check_state(self):
         # 終了しているかどうかをチェックする
@@ -18,16 +18,16 @@ class MiniBoard:
         bingo = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6),
                  (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
         for a, b, c in bingo:
-            if self.mini_board[a] != 0 and self.mini_board[a] == self.mini_board[b] == self.mini_board[c]:
-                self.state = self.mini_board[a]
+            if self.local_board[a] != 0 and self.local_board[a] == self.local_board[b] == self.local_board[c]:
+                self.state = self.local_board[a]
                 return self.state
-        if 0 not in self.mini_board:
+        if 0 not in self.local_board:
             self.state = 3
             return self.state
         return 0
 
     def __init__(self):
-        self.mini_board = [0] * 9
+        self.local_board = [0] * 9
         self.state = 0
 
 
@@ -35,7 +35,7 @@ class Board:
     # 盤面
     def flatten(self):
         # 現在の盤面を整数のリストにする
-        board = sum([mini_board.flatten() for mini_board in  self.mini_boards], [])
+        board = sum([local_board.flatten() for local_board in  self.local_boards], [])
         return board
 
     def legal_moves(self):
@@ -46,21 +46,21 @@ class Board:
     def check_state(self):
         # 終了しているかどうかをチェックする
         # 0:続行 1:プレイヤー1の勝ち 2:プレイヤー2の勝ち 3:引き分け
-        return self.big_board.check_state()
+        return self.grobal_board.check_state()
 
-    def board_to_miniboard(self, pos):
-        # boardのindexを(miniboard番号, miniboardのindex)に変換する
-        mini_num, mini_pos = pos / 9, pos % 9
-        return (mini_num, mini_pos)
+    def board_to_localboard(self, pos):
+        # boardのindexを(localboard番号, localboardのindex)に変換する
+        local_num, local_pos = pos / 9, pos % 9
+        return (local_num, local_pos)
 
     def mark(self, pos, player):
         # idx番目のマス目にplayerの手を打つ
-        mini_num, mini_pos = self.board_to_miniboard(pos)
-        self.mini_boards[mini_num].mark(mini_pos, player)
-        mini_result = self.mini_boards[mini_num].check_state()
-        if mini_result != 0:
-            self.big_board.mark(mini_num, player)
+        local_num, local_pos = self.board_to_localboard(pos)
+        self.local_boards[local_num].mark(local_pos, player)
+        local_result = self.local_boards[local_num].check_state()
+        if local_result != 0:
+            self.grobal_board.mark(local_num, player)
 
     def __init__(self):
-        self.mini_boards = [MiniBoard() for _ in range(9)]  # 3×3の小盤面
-        self.big_board = MiniBoard()    # 大きな盤面
+        self.local_boards = [LocalBoard() for _ in range(9)]  # 3×3の小盤面
+        self.grobal_board = LocalBoard()    # 大きな盤面
