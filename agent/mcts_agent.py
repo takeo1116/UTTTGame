910b38615cbba2677e2231418cbcc3e783389170
takeo1116 <takeo1116@gmail.com>
2020-11-17 22:47:28 +0900

    mcts wip

--- agent/mcts_agent.py
+++ agent/mcts_agent.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 
 import random
+import copy
 from .agentbase import AgentBase
 from engine.board import Board
 
@@ -9,6 +10,12 @@ class MctsAgent(AgentBase):
         # 現在の盤面から決まった回数プレイアウトして、一番良い手を返す
         root_board = Board(board)
         
+        # とりあえず100回プレイアウト
+        for play in range(100):
+            now_board = copy.deepcopy(root_board)
+            
+            # 
+
         return random.choice(legal)
 
     def game_end(self, board, player_num, result):
